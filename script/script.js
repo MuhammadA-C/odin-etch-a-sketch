20b7001bc6003c622f7c388bc7e82789cd89e586
MuhammadA-C <phantomganhdi@gmail.com>
2023-07-27 20:43:52 -0700

    Reset Button
    Created the button to reset the grid color.

--- script/script.js
+++ script/script.js
@@ -8,25 +8,42 @@
 const gridContainerWidth = 960;
 const divBorderWidth = 2;
 const grid = document.querySelector("#grid");
+const button = document.querySelector("button");
 
 let size = 16;
 let gridSize = (size * size);
 
-
-
-
+//Dynamically creates the size of the grid
 for (let i = 0; i < gridSize; i++) {
   const div = document.createElement("div");
   const diveSize = (gridContainerWidth / size) - divBorderWidth;
 
   div.style.width = `${diveSize}px`;
   div.style.height = `${diveSize}px`;
-
-  div.addEventListener("mouseenter", () => {
-    div.style.backgroundColor = "black";
-  });
-
   grid.appendChild(div);
+}
+
+const divs = document.querySelectorAll("#grid > div");
+
+//Changes grid squares color if mouse hovers over it
+for(let i = 0; i < divs.length; i++) {
+  divs[i].addEventListener("mouseenter", () => {
+    divs[i].style.backgroundColor = "black";
+  });
+}
+
+//Resets the color of the grid squares if the reset button is clicked
+button.addEventListener("click", () => {
+  turnOffBtnEffect();
+  setTimeout(turnOffBtnEffect, 100);
+
+  for(let i = 0; i < divs.length; i++) {
+    divs[i].style.backgroundColor = "white";
+  }
+});
+
+function turnOffBtnEffect() {
+  button.classList.toggle("button-hover-effect");
 }
 
 
