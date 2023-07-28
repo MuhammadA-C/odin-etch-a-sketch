6aded1cddd7dbb7c3f6c3f65de82fd1a1af0be50
MuhammadA-C <phantomganhdi@gmail.com>
2023-07-27 22:21:04 -0700

    Finished
    Completed the etch-a-sktch project.
    
    Note: Future improvement would be to re-design the way the user changes the grid size, and to include a slider instead of the button that I currently have.

--- script/script.js
+++ script/script.js
@@ -1,29 +1,24 @@
 /*
-
   Name: Muhammad Chambers
-  Date: 
+  Date: 07/27/2023
 
+  Overview: The Odin Project - Foundations : Etch-A-Sketch Project
+  
+  For this project we were supposed to create a dynamica etch-a-sketch board that changes
+  its size based on user input ranging from 1-100. And as the user hovers over a grid cell,
+  the cell will change colors.
 */
 
 const gridContainerWidth = 960;
 const divBorderWidth = 2;
 const grid = document.querySelector("#grid");
-const button = document.querySelector("button");
-
-let size = 16;
-let gridSize = (size * size);
+const resetBtn = document.querySelector(".btn-reset");
+const changeGridSizeBtn = document.querySelector(".btn-grid-size");
 
 //Dynamically creates the size of the grid
-for (let i = 0; i < gridSize; i++) {
-  const div = document.createElement("div");
-  const diveSize = (gridContainerWidth / size) - divBorderWidth;
+createGrid();
 
-  div.style.width = `${diveSize}px`;
-  div.style.height = `${diveSize}px`;
-  grid.appendChild(div);
-}
-
-const divs = document.querySelectorAll("#grid > div");
+let divs = document.querySelectorAll("#grid > div");
 
 //Changes grid squares color if mouse hovers over it
 for(let i = 0; i < divs.length; i++) {
@@ -33,8 +28,8 @@ for(let i = 0; i < divs.length; i++) {
 }
 
 //Resets the color of the grid squares if the reset button is clicked
-button.addEventListener("click", () => {
-  turnOffBtnEffect();
+resetBtn.addEventListener("click", () => {
+  turnOffBtnEffect(resetBtn);
   setTimeout(turnOffBtnEffect, 100);
 
   for(let i = 0; i < divs.length; i++) {
@@ -42,8 +37,37 @@ button.addEventListener("click", () => {
   }
 });
 
-function turnOffBtnEffect() {
-  button.classList.toggle("button-hover-effect");
+changeGridSizeBtn.addEventListener("click", () => {
+  location.reload();
+});
+
+function turnOffBtnEffect(button) {
+  button.classList.toggle("btn-hover-effect");
+}
+
+function createGrid() {
+  let isInputValid = false;
+  let divsPerRow;
+
+  while(!isInputValid) {
+    let userInput = prompt("Enter a number between 1-100 for the grid size."); 
+
+    if(userInput > 0 && userInput <= 100) {
+      divsPerRow = userInput;
+      break;
+    }
+  }
+
+  const gridSize = (divsPerRow * divsPerRow);
+
+  for (let i = 0; i < gridSize; i++) {
+    const div = document.createElement("div");
+    const diveSize = (gridContainerWidth / divsPerRow) - divBorderWidth;
+  
+    div.style.width = `${diveSize}px`;
+    div.style.height = `${diveSize}px`;
+    grid.appendChild(div);
+  }
 }
 
 
