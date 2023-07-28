0e9797796be2e0ff336a79a6a63f8cededf3a9a2
MuhammadA-C <phantomganhdi@gmail.com>
2023-07-27 19:54:01 -0700

    Grid Generation
    Created the code that can take any number and generate a grid based on said number.

--- script/script.js
+++ script/script.js
@@ -1,1 +1,26 @@
+/*
 
+  Name: Muhammad Chambers
+  Date: 
+
+*/
+
+const gridContainerWidth = 960;
+const divBorderWidth = 2;
+const grid = document.querySelector("#grid");
+
+let size = 16;
+let gridSize = (size * size);
+
+
+
+
+for (let i = 0; i < gridSize; i++) {
+  const div = document.createElement("div");
+  const diveSize = (gridContainerWidth / size) - divBorderWidth;
+
+  div.style.width = `${diveSize}px`;
+  div.style.height = `${diveSize}px`;
+
+  grid.appendChild(div);
+}
